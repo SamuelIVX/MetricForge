266db9165c16cc9db888403f8ac8ee88bafcc841
Samuel <samuel05.hb@gmail.com>
2026-01-17 17:46:29 -0500

    fix: Reverted back the 'MainContent' page after it was accidentally changed.

--- app/tasks/[taskId]/components/page.tsx
+++ app/tasks/[taskId]/components/page.tsx
@@ -3,8 +3,14 @@ import RightColumnContent from "./right_column/page"
 
 export default function MainContent() {
     return (
-        <div>
-            Main Content
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+
+            {/* Left Column */}
+            <LeftColumnContent />
+
+            {/* Right Column */}
+            <RightColumnContent />
+
         </div>
     )
 }
