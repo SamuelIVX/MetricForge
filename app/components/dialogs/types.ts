2dc0a20beb372ca5d51ec52acdd1d38cff40b7f4
Samuel <samuel05.hb@gmail.com>
2026-01-17 00:33:27 -0500

    fix: Fixed import.

--- app/components/dialogs/types.ts
+++ app/components/dialogs/types.ts
@@ -1,4 +1,4 @@
-import { TaskDetails } from "../tables/task_table/columns";
+import { TaskDetails } from "../tables/task_table/types";
 
 export type Props = {
     onAdd: (task: TaskDetails) => void;
