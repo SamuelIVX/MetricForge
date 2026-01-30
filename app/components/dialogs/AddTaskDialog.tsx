991347049a39d30872a251dbf0526371c1136612
Samuel <samuel05.hb@gmail.com>
2026-01-29 19:10:56 -0500

    fix: Switching to absolute imports to try to fix the SuperLinter error stating that the types.ts file is not being located (Seems like it works locally but not for this CI workflow).

--- app/components/dialogs/AddTaskDialog.tsx
+++ app/components/dialogs/AddTaskDialog.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { TaskDetails } from "../tables/task_table/types";
+import { TaskDetails } from "@/app/components/tables/task_table/types"
 
 import {
     Dialog,
--- app/components/dialogs/types.ts
+++ app/components/dialogs/types.ts
@@ -1,4 +1,4 @@
-import { TaskDetails } from "../tables/task_table/types";
+import { TaskDetails } from "@/app/components/tables/task_table/types";
 
 export type Props = {
     onAdd: (task: TaskDetails) => void;
