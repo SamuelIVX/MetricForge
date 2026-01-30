891e1e48ca7a6c19849d0420775504ac5ab4ad4b
Samuel <samuel05.hb@gmail.com>
2026-01-30 00:44:30 -0500

    fix: disabled 'no/no-missing-import' for TypeScript path aliases.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,11 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    rules: {
+      'n/no-missing-import': 'off',
+    }
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
