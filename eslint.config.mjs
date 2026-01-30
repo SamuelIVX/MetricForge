e33a389a32dcd5d14134e554fb5cfc5d039e574f
Samuel <samuel05.hb@gmail.com>
2026-01-30 00:36:07 -0500

    fix: configure ESLint to resolve TypeScript path aliases

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,31 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+// @ts-expect-error - ESLint types
+import importX from "eslint-plugin-import-x";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  
+  {
+    plugins: {
+      'import-x': importX,
+    },
+    settings: {
+      'import-x/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project: './tsconfig.json',
+        },
+      },
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
