d49c7cbfdcc73d93e872d30a4d935cd434ec1476
Samuel <samuel05.hb@gmail.com>
2026-01-30 00:40:37 -0500

    fix: Reverting changes since it did not fix SuperLint issue.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,13 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
-// @ts-expect-error - ESLint types
-import importX from "eslint-plugin-import-x";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  
-  {
-    plugins: {
-      'import-x': importX,
-    },
-    settings: {
-      'import-x/resolver': {
-        typescript: {
-          alwaysTryTypes: true,
-          project: './tsconfig.json',
-        },
-      },
-      react: {
-        version: 'detect',
-      },
-    },
-  },
-  
+  // Override default ignores of eslint-config-next.
   globalIgnores([
+    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
